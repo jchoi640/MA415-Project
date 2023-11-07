7b316a037d1bcacd9e2a7456a4634f7adf80ba84
AngelWuSY <wangel@bu.edu>
2023-11-06 19:04:53 -0500

    smaller sample

--- scripts/birth_load_and_clean_data.R
+++ scripts/birth_load_and_clean_data.R
@@ -8,9 +8,12 @@ output: html_document
 birthdata<-read.csv("nat2021us.csv")
 
 library(dplyr)
+set.seed(1000)
 birth <- birthdata |>
   filter(tbo_rec==1)|>
   #first child
-  select(mager, mrace6,dmar, meduc, cig_0, m_ht_in,bmi,pwgt_r,dwgt_r,rf_pdiab,dbwt,rcity_pop)
+  select(mager, mrace6,dmar, meduc, cig_0, m_ht_in,bmi,pwgt_r,dwgt_r,rf_pdiab,dbwt)
 #age,race, education, daily smoke before pregnancy, height in inches, bmi, weight in pound before pregancy, weight in pound when delivered, pre-diabetes, Birth Weight , mother live city population)
-  
+  sample_size <- 100000  # Adjust the sample size as needed
+  birth_sample <- birth |>
+  sample_n(size = sample_size, replace = FALSE)
